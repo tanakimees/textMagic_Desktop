55f029edc203fc07f5a2a4ad5263e2292faa2b7f
Kristo Tänak <tanak.kristo@gmail.com>
2022-05-18 11:04:35 +0200

    Add project files.

---
+++ textMagic_Desktop/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace textMagic_Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fadeForm = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 60);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1280, 660);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(20, 20);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1240, 620);
+            this.panel2.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.label5.Location = new System.Drawing.Point(821, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(313, 40);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Save login information:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(135)))), ((int)(((byte)(185)))));
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel3.Location = new System.Drawing.Point(1140, 133);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(80, 80);
+            this.panel3.TabIndex = 8;
+            this.panel3.Click += new System.EventHandler(this.panel3_Click);
+            this.panel3.MouseEnter += new System.EventHandler(this.panel3_MouseEnter);
+            this.panel3.MouseLeave += new System.EventHandler(this.panel3_MouseLeave);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.panel4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel4.Location = new System.Drawing.Point(10, 10);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(60, 60);
+            this.panel4.TabIndex = 9;
+            this.panel4.Click += new System.EventHandler(this.panel4_Click);
+            this.panel4.MouseEnter += new System.EventHandler(this.panel4_MouseEnter);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(135)))), ((int)(((byte)(185)))));
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.label4.Location = new System.Drawing.Point(340, 133);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label4.Size = new System.Drawing.Size(300, 80);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Create API key";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            this.label4.MouseEnter += new System.EventHandler(this.label4_MouseEnter);
+            this.label4.MouseLeave += new System.EventHandler(this.label4_MouseLeave);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(135)))), ((int)(((byte)(185)))));
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(20, 133);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.label3.Size = new System.Drawing.Size(300, 80);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Login";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label3.MouseEnter += new System.EventHandler(this.label3_MouseEnter);
+            this.label3.MouseLeave += new System.EventHandler(this.label3_MouseLeave);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(135)))), ((int)(((byte)(185)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(920, 77);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(0, 2, 0, 0);
+            this.label2.Size = new System.Drawing.Size(300, 36);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "API key";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 20.25F);
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(20, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '●';
+            this.textBox2.Size = new System.Drawing.Size(880, 36);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(20, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(880, 36);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(135)))), ((int)(((byte)(185)))));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(920, 20);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 2, 0, 0);
+            this.label1.Size = new System.Drawing.Size(300, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // fadeForm
+            // 
+            this.fadeForm.Interval = 1;
+            this.fadeForm.Tick += new System.EventHandler(this.fadeForm_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::textMagic_Desktop.Properties.Resources.lineselected;
+            this.pictureBox2.Location = new System.Drawing.Point(1185, 10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            this.pictureBox2.MouseLeave += new System.EventHandler(this.pictureBox2_MouseLeave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::textMagic_Desktop.Properties.Resources.closeselected;
+            this.pictureBox1.Location = new System.Drawing.Point(1230, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox3.Image = global::textMagic_Desktop.Properties.Resources.logo_textmagic;
+            this.pictureBox3.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(159, 40);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.Text = "TextMagic";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer fadeForm;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ textMagic_Desktop/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TextmagicRest;
+using TextmagicRest.Model;
+
+namespace textMagic_Desktop
+{
+    public partial class Form1 : Form
+    {
+        //Local data
+        bool fadeInOrOut = false;
+        bool saveData = false;
+
+        //Imports necessary DLL file for drawing rounded corners.
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
+        //InitializeComponent()
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Loads the form in.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateRoundedCorners(this, 30);
+            CreateRoundedCorners(pictureBox1, 20);
+            CreateRoundedCorners(pictureBox2, 20);
+            CreateRoundedCorners(panel2, 20);
+            CreateRoundedCorners(panel4, 60);
+            CreateRoundedCorners(panel3, 80);
+            CreateRoundedCorners(textBox1, 5);
+            CreateRoundedCorners(textBox2, 5);
+            CreateRoundedCorners(label1, 5);
+            CreateRoundedCorners(label2, 5);
+            CreateRoundedCorners(label3, 10);
+            CreateRoundedCorners(label4, 10);
+            CreateRoundedCorners(label5, 10);
+            fadeForm.Start();
+
+            if(File.Exists(@"C:\temp\TextMagicLogin.txt"))
+            {
+                StreamReader sr = new StreamReader(@"C:\temp\TextMagicLogin.txt");
+                string username1 = sr.ReadLine();
+                string apikey = sr.ReadLine();
+
+                textBox1.Text = username1;
+                textBox2.Text = apikey;
+
+                panel4.BackColor = Color.White;
+                saveData = true;
+
+                sr.Close();
+            }          
+        }
+
+        //Optimizes the creation of rounded corners.
+        void CreateRoundedCorners(Control c, int x)
+        {
+            c.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, c.Width, c.Height, x, x));
+        }
+
+        //Allows the borderless form to be moveable.
+        protected override void WndProc(ref System.Windows.Forms.Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        //Fades the form in or out, dependent on the 'fadeInOrOut' boolean.
+        private void fadeForm_Tick(object sender, EventArgs e)
+        {
+            switch(fadeInOrOut)
+            {
+                case false:
+                    this.Opacity += 0.01;
+                    if(this.Opacity == 1)
+                    {
+                        fadeForm.Stop();
+                    }
+                    break;
+                case true:
+                    this.Opacity -= 0.01;
+                    if (this.Opacity == 0)
+                    {
+                        this.Close();
+                        Application.Exit();
+                        fadeForm.Stop();
+                    }
+                    break;
+            }
+        }
+
+        //Close button MouseEnter animation.
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.FromArgb(42, 135, 185);
+        }
+
+        //Close button MouseLeave animation.
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.FromArgb(0,92,138);
+        }
+
+        // Close button click handler.
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            fadeForm.Stop();
+            fadeInOrOut = true;
+            fadeForm.Start();
+        }
+
+        //Minimize to tray button MouseEnter animation,
+        private void pictureBox2_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox2.BackColor = Color.FromArgb(42, 135, 185);
+        }
+
+        //Minimize to tray button MouseLeave animation.
+        private void pictureBox2_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox2.BackColor = Color.FromArgb(0, 92, 138);
+        }
+
+        //Minimize to tray button click handler.
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        //Login button MouseEnter animation.
+        private void label3_MouseEnter(object sender, EventArgs e)
+        {
+            label3.BackColor = Color.FromArgb(62, 155, 205);
+        }
+
+        //Login button MouseLeave animation.
+        private void label3_MouseLeave(object sender, EventArgs e)
+        {
+            label3.BackColor = Color.FromArgb(42, 135, 185);
+        }
+
+        //Login button click.
+        private void label3_Click(object sender, EventArgs e)
+        {
+            var client = new Client(textBox1.Text, textBox2.Text);
+            var auth = client.Ping();
+
+            if(auth.Success)
+            {
+                MessageBox.Show("success");
+            }
+            else
+            {
+                MessageBox.Show("fail");
+            }
+        }
+
+        //Create API key button MouseEnter animation.
+        private void label4_MouseEnter(object sender, EventArgs e)
+        {
+            label4.BackColor = Color.FromArgb(62, 155, 205);
+        }
+
+        //Create API key button MouseLeave animation.
+        private void label4_MouseLeave(object sender, EventArgs e)
+        {
+            label4.BackColor = Color.FromArgb(42, 135, 185);
+        }
+
+        //Create API key button click.
+        private void label4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://my.textmagic.com/online/api/rest-api/keys-trial");
+        }
+
+        private void panel3_MouseEnter(object sender, EventArgs e)
+        {
+            panel4.BackColor = Color.White;
+        }
+
+        private void panel3_MouseLeave(object sender, EventArgs e)
+        {
+            if(saveData == true)
+            {
+                panel4.BackColor = Color.White;
+            }
+            else
+            {
+                panel4.BackColor = Color.FromArgb(0, 92, 138);
+            }
+        }
+
+        private void panel4_MouseEnter(object sender, EventArgs e)
+        {
+            panel4.BackColor = Color.White;
+        }
+
+        private void panel3_Click(object sender, EventArgs e)
+        {
+            if(saveData == false)
+            {
+                saveData = true;
+                saveLogin();
+                panel4.BackColor = Color.White;
+            }
+            else
+            {
+                saveData = false;
+                saveLogin();
+            }
+        }
+
+        private void panel4_Click(object sender, EventArgs e)
+        {
+            if (saveData == false)
+            {
+                saveData = true;
+                saveLogin();
+                panel4.BackColor = Color.White;
+            }
+            else
+            {
+                saveData = false;
+                saveLogin();
+            }
+        }
+
+        void saveLogin()
+        {
+            switch(saveData)
+            {
+                case false:
+                    if(File.Exists(@"C:\temp\TextMagicLogin.txt"))
+                    {
+                        File.Delete(@"C:\temp\TextMagicLogin.txt");
+                    }
+                    break;
+                case true:
+                    if (!File.Exists(@"C:\temp\TextMagicLogin.txt"))
+                    {
+                        using(StreamWriter sw = File.CreateText(@"C:\temp\TextMagicLogin.txt"))
+                        {
+                            sw.WriteLine(textBox1.Text);
+                            sw.WriteLine(textBox2.Text);
+
+                            sw.Dispose();
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
